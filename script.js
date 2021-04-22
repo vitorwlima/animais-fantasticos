bc04e553723bd567e410dae4c8ace3ad3dcdfb0a
vitorwlima <vitorwlima13@gmail.com>
2021-04-22 19:33:22 -0300

    código do scrollSuave separado em função para organização

--- script.js
+++ script.js
@@ -36,18 +36,21 @@ function initFaqInteraction() {
 }
 initFaqInteraction();
 
-const linksInternos = document.querySelectorAll("a[href^='#']");
+function scrollSuave() {
+  const linksInternos = document.querySelectorAll("a[href^='#']");
 
-function scrollToSection(event) {
-  event.preventDefault();
-  const href = this.getAttribute("href");
-  const section = document.querySelector(href);
-  section.scrollIntoView({
-    behavior: "smooth",
-    block: "start",
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = this.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
   });
 }
-
-linksInternos.forEach((link) => {
-  link.addEventListener("click", scrollToSection);
-});
+scrollSuave();
