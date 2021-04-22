e2bc39dc79727a08623faf0d0944480e74d74c40
vitorwlima <vitorwlima13@gmail.com>
2021-04-22 17:54:33 -0300

    adicionado interatividade para esconder e mostrar as perguntas no FAQ

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-const tabMenu = document.querySelectorAll(".animais-lista li");
-const tabContent = document.querySelectorAll(".animais-descricao section");
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".animais-lista li");
+  const tabContent = document.querySelectorAll(".animais-descricao section");
 
-function definirAnimal(index) {
-  tabContent.forEach((item) => {
-    item.classList.remove("ativo");
+  function definirAnimal(index) {
+    tabContent.forEach((item) => {
+      item.classList.remove("ativo");
+    });
+    tabContent[index].classList.add("ativo");
+  }
+
+  tabMenu.forEach((item, index) => {
+    item.addEventListener("click", function () {
+      definirAnimal(index);
+    });
   });
-  tabContent[index].classList.add("ativo");
 }
+initTabNav();
 
-tabMenu.forEach((item, index) => {
-  item.addEventListener("click", function () {
-    definirAnimal(index);
+function initFaqInteraction() {
+  const tabPerguntas = document.querySelectorAll("dt");
+  const tabRespostas = document.querySelectorAll("dd");
+
+  function pickQuestion(index) {
+    tabRespostas[index].classList.toggle("ativo");
+    tabPerguntas[index].classList.toggle("ativo");
+  }
+
+  tabPerguntas.forEach((item, index) => {
+    item.addEventListener("click", function () {
+      pickQuestion(index);
+    });
   });
-});
+  tabPerguntas[0].classList.add("ativo");
+  tabRespostas[0].classList.add("ativo");
+}
+initFaqInteraction();
